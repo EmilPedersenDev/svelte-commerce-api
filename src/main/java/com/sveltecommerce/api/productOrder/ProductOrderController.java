3b7eb42c36aa5deb5f7aa6fbaf0cc34baa7a25cd
RefinedWiki Build Agent <bamboo@refinedwiki.com>
2023-09-03 21:08:04 +0200

    adding backend for completing order

--- src/main/java/com/sveltecommerce/api/productOrder/ProductOrderController.java
+++ src/main/java/com/sveltecommerce/api/productOrder/ProductOrderController.java
@@ -6,10 +6,8 @@ import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.dao.DataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -21,77 +19,102 @@ import org.springframework.web.server.ResponseStatusException;
 
 import javax.ws.rs.Consumes;
 import javax.ws.rs.Produces;
-import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 import java.util.List;
-import java.util.Optional;
 
 import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
 
 @RestController
-@Consumes({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
+@Consumes({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
 @RequestMapping("${apiVersion}" + "productOrders")
 public class ProductOrderController {
-    private final ProductOrderRepository repository;
+  private final ProductOrderRepository repository;
 
-    private final OrderItemRepository orderItemRepository;
+  private final OrderItemRepository orderItemRepository;
 
-    private final ProductItemRepository productItemRepository;
+  private final ProductItemRepository productItemRepository;
 
-    private final ProductOrderService productOrderService;
+  private final ProductOrderService productOrderService;
 
-    private final Logger logger;
+  private final Logger logger;
 
-    public ProductOrderController(ProductOrderRepository repository, OrderItemRepository orderItemRepository, ProductItemRepository productItemRepository, ProductOrderService productOrderService) {
-        this.repository = repository;
-        this.orderItemRepository = orderItemRepository;
-        this.productItemRepository = productItemRepository;
-        this.productOrderService = productOrderService;
-        this.logger = LoggerFactory.getLogger(ProductOrderController.class);
+  public ProductOrderController(ProductOrderRepository repository, OrderItemRepository orderItemRepository, ProductItemRepository productItemRepository, ProductOrderService productOrderService) {
+    this.repository = repository;
+    this.orderItemRepository = orderItemRepository;
+    this.productItemRepository = productItemRepository;
+    this.productOrderService = productOrderService;
+    this.logger = LoggerFactory.getLogger(ProductOrderController.class);
+  }
+
+  @GetMapping
+  @Produces(APPLICATION_JSON)
+  public ResponseEntity<List<ProductOrder>> getPorductOrders() {
+    try {
+      List<ProductOrder> productOrders = repository.findAll();
+      return new ResponseEntity<>(productOrders, HttpStatus.OK);
+    } catch (Exception ex) {
+      throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, ex.getMessage());
     }
+  }
 
-    @GetMapping
-    @Produces(APPLICATION_JSON)
-    public ResponseEntity<List<ProductOrder>> getPorductOrders() {
-        try {
-            List<ProductOrder> productOrders = repository.findAll();
-            return new ResponseEntity<>(productOrders, HttpStatus.OK);
-        } catch (Exception ex) {
-            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, ex.getMessage());
-        }
+  @GetMapping("/{id}")
+  @Produces(APPLICATION_JSON)
+  public ResponseEntity<ProductOrder> getOrder(@PathVariable long id, @NotNull @RequestParam String status) {
+    try {
+      if (id < 0) {
+        throw new RuntimeException("No valid id");
+      }
+      List<ProductOrder> productOrders = repository.findByUserIdAndStatus(id, status);
+      ProductOrder pendingProductOrder = productOrders.stream().filter(order -> order.getStatus().equals(ProductOrderStatus.PENDING.status)).findFirst().orElse(null);
+      return new ResponseEntity<>(pendingProductOrder, HttpStatus.OK);
+    } catch (Exception ex) {
+      throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, ex.getMessage());
     }
+  }
 
-    @GetMapping("/{id}")
-    @Produces(APPLICATION_JSON)
-    public ResponseEntity<ProductOrder> getOrder(@PathVariable long id, @NotNull @RequestParam String status) {
-        try {
-            if(id < 0) {
-                throw new RuntimeException("No valid id");
-            }
-            List<ProductOrder> productOrders = repository.findByUserIdAndStatus(id, status);
-            ProductOrder pendingProductOrder = productOrders.stream().filter(order -> order.getStatus().equals(ProductOrderStatus.PENDING.status)).findFirst().orElse(null);
-            return new ResponseEntity<>(pendingProductOrder, HttpStatus.OK);
-        } catch(Exception ex) {
-            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, ex.getMessage());
-        }
+
+  @PostMapping
+  @Produces(APPLICATION_JSON)
+  public ResponseEntity<ProductOrder> createOrder(@Valid @RequestBody CreateProductOrderRequest productOrderRequest) {
+    if (productOrderRequest.getProductItem() == null) {
+      throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Not a valid order request");
     }
-
-
-    @PostMapping
-    @Produces(APPLICATION_JSON)
-    public ResponseEntity<ProductOrder> createOrder(@Valid @RequestBody CreateProductOrderRequest productOrderRequest) {
-        if(productOrderRequest.getProductItem() == null) {
-            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Not a valid order request");
-        }
-        try {
-            ProductOrder orderCreatedResponse = productOrderService.createOrder(productOrderRequest);
-            return new ResponseEntity<>(orderCreatedResponse, HttpStatus.CREATED);
-        } catch(RuntimeException e) {
-            logger.error(e.getMessage());
-            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, e.getMessage());
-        } catch (Exception e) {
-            logger.error(e.getMessage());
-            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Could not save order");
-        }
+    try {
+      ProductOrder orderCreatedResponse = productOrderService.createOrder(productOrderRequest);
+      return new ResponseEntity<>(orderCreatedResponse, HttpStatus.CREATED);
+    } catch (RuntimeException e) {
+      logger.error(e.getMessage());
+      throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, e.getMessage());
+    } catch (Exception e) {
+      logger.error(e.getMessage());
+      throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Could not save order");
     }
+  }
+
+  @PostMapping("/checkout")
+  @Produces(APPLICATION_JSON)
+  public ResponseEntity<String> checkout(@Valid @RequestBody ProductOrder productOrder) {
+    try {
+      String redirectUrl = productOrderService.checkoutStripe(productOrder);
+      return new ResponseEntity<>(redirectUrl, HttpStatus.OK);
+    } catch (RuntimeException e) {
+      logger.error(e.getMessage());
+      throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, e.getMessage());
+    } catch (Exception e) {
+      logger.error(e.getMessage());
+      throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Checkout was not successful");
+    }
+  }
+
+  @PostMapping("/complete-order")
+  @Produces(APPLICATION_JSON)
+  public ResponseEntity<Long> completeOrder(@Valid @RequestBody ProductOrder productOrder) {
+    try {
+      ProductOrder completedOrder = productOrderService.completeOrder(productOrder);
+      return new ResponseEntity<>(completedOrder.getId(), HttpStatus.OK);
+    } catch (Exception e) {
+      logger.error(e.getMessage());
+      throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Could not save order");
+    }
+  }
 }
--- src/main/java/com/sveltecommerce/api/productOrder/ProductOrderService.java
+++ src/main/java/com/sveltecommerce/api/productOrder/ProductOrderService.java
@@ -1,5 +1,6 @@
 package com.sveltecommerce.api.productOrder;
 
+import com.stripe.exception.StripeException;
 import com.sveltecommerce.api.orderItem.OrderItem;
 import com.sveltecommerce.api.orderItem.OrderItemRepository;
 import com.sveltecommerce.api.product.Product;
@@ -8,108 +9,181 @@ import com.sveltecommerce.api.productItem.ProductItem;
 import com.sveltecommerce.api.productItem.ProductItemRepository;
 import com.sveltecommerce.api.user.User;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.dao.DataAccessException;
 import org.springframework.stereotype.Component;
 
+import java.io.IOException;
 import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 import java.util.Optional;
+
+import static spark.Spark.port;
+
+import com.stripe.Stripe;
+import com.stripe.model.checkout.Session;
+import com.stripe.param.checkout.SessionCreateParams;
 
 @Component
 public class ProductOrderService {
 
-    private final ProductOrderRepository repository;
+  private final ProductOrderRepository repository;
 
-    private final ProductItemRepository productItemRepository;
+  private final ProductItemRepository productItemRepository;
 
-    private final OrderItemRepository orderItemRepository;
+  private final OrderItemRepository orderItemRepository;
 
-    private final ProductRepository productRepository;
-    @Autowired
-    public ProductOrderService(ProductOrderRepository repository, ProductItemRepository productItemRepository, OrderItemRepository orderItemRepository, ProductRepository productRepository) {
-        this.repository = repository;
-        this.productItemRepository = productItemRepository;
-        this.orderItemRepository = orderItemRepository;
-        this.productRepository = productRepository;
+  private final ProductRepository productRepository;
+
+  @Autowired
+  public ProductOrderService(ProductOrderRepository repository, ProductItemRepository productItemRepository, OrderItemRepository orderItemRepository, ProductRepository productRepository) {
+    this.repository = repository;
+    this.productItemRepository = productItemRepository;
+    this.orderItemRepository = orderItemRepository;
+    this.productRepository = productRepository;
+  }
+
+  public ProductOrder createOrder(CreateProductOrderRequest productOrderRequest) throws RuntimeException {
+    ProductItem productItemReq = productOrderRequest.getProductItem();
+
+    // Temporary user for testing purposes
+    User testUser = new User(productOrderRequest.getUserId(), "Emil Pedersen");
+    ProductOrder pendingProductOrder = getPendingProductOrder(testUser.getId());
+
+    if (pendingProductOrder == null) {
+      pendingProductOrder = new ProductOrder(ProductOrderStatus.PENDING.status, productOrderRequest.getUserId());
+      repository.save(pendingProductOrder);
     }
 
-    public ProductOrder createOrder(CreateProductOrderRequest productOrderRequest) throws RuntimeException {
-        ProductItem productItemReq = productOrderRequest.getProductItem();
+    Optional<ProductItem> savedProductItem = productItemRepository.findById(productItemReq.getId());
 
-        // Temporary user for testing purposes
-        User testUser = new User(productOrderRequest.getUserId(), "Emil Pedersen");
-        ProductOrder pendingProductOrder = getPendingProductOrder(testUser.getId());
+    // The stock of the productItem needs to be reduced when creating a new order
+    int productItemQuantity = getProductItemQuantity(savedProductItem, productItemReq);
+    savedProductItem.ifPresent(item -> item.setQuantity(productItemQuantity));
 
-       if(pendingProductOrder == null) {
-            pendingProductOrder = new ProductOrder(ProductOrderStatus.PENDING.status, productOrderRequest.getUserId());
-            repository.save(pendingProductOrder);
-        }
+    OrderItem newOrderItem = createOrderItem(productItemReq, pendingProductOrder);
+    if (newOrderItem != null) {
+      pendingProductOrder.addOrderItem(newOrderItem);
+    }
+    updateProductItem(savedProductItem);
 
-        Optional<ProductItem> savedProductItem = productItemRepository.findById(productItemReq.getId());
+    return pendingProductOrder;
+  }
 
-        // The stock of the productItem needs to be reduced when creating a new order
-        int productItemQuantity = getProductItemQuantity(savedProductItem, productItemReq);
-        savedProductItem.ifPresent(item -> item.setQuantity(productItemQuantity));
+  private OrderItem createOrderItem(ProductItem productItemReq, ProductOrder productOrderReq) throws RuntimeException {
+    List<OrderItem> savedOrderItems = orderItemRepository.findByProductItemId(productItemReq.getId());
+    Optional<OrderItem> savedOrderItem = savedOrderItems.stream().filter(item -> item.getProductOrder().getId() == productOrderReq.getId()).findFirst();
 
-        OrderItem newOrderItem = createOrderItem(productItemReq, pendingProductOrder);
-        if(newOrderItem != null) {
-            pendingProductOrder.addOrderItem(newOrderItem);
-        }
-        updateProductItem(savedProductItem);
+    // We only want to update the quantity when the savedOrderItem already exists
+    OrderItem newOrderItem = null;
 
-        return pendingProductOrder;
+    Optional<Product> product = productRepository.findById(productItemReq.getProductId());
+    BigDecimal productPrice = null;
+
+    if (product.isPresent()) {
+      productPrice = product.get().getPrice();
+    } else {
+      throw new RuntimeException("Product was not found");
     }
 
-    private OrderItem createOrderItem(ProductItem productItemReq, ProductOrder productOrderReq) throws RuntimeException {
-        List<OrderItem> savedOrderItems = orderItemRepository.findByProductItemId(productItemReq.getId());
-        Optional<OrderItem> savedOrderItem = savedOrderItems.stream().filter(item -> item.getProductOrder().getId() == productOrderReq.getId()).findFirst();
+    if (savedOrderItem.isPresent()) {
+      savedOrderItem.get().setQuantity(savedOrderItem.get().getQuantity() + productItemReq.getQuantity());
+      BigDecimal orderItemPrice = productPrice.multiply(new BigDecimal(productItemReq.getQuantity()));
+      savedOrderItem.get().addPrice(orderItemPrice);
+      orderItemRepository.save(savedOrderItem.get());
+    } else {
+      OrderItem orderItem = new OrderItem(productItemReq.getQuantity(), productItemReq, productOrderReq, productPrice.multiply(new BigDecimal(productItemReq.getQuantity())));
+      newOrderItem = orderItemRepository.save(orderItem);
+    }
 
-        // We only want to update the quantity when the savedOrderItem already exists
-        OrderItem newOrderItem = null;
+    return newOrderItem;
+  }
 
-        Optional<Product> product = productRepository.findById(productItemReq.getProductId());
-        BigDecimal productPrice = null;
+  private void updateProductItem(Optional<ProductItem> savedProductItem) {
+    savedProductItem.ifPresent(newProductItem -> productItemRepository.save(savedProductItem.get()));
+  }
 
-        if(product.isPresent()) {
-            productPrice = product.get().getPrice();
+
+  private int getProductItemQuantity(Optional<ProductItem> savedProductItem, ProductItem productItemReq) throws RuntimeException {
+    if (savedProductItem.isPresent() && savedProductItem.get().getQuantity() > 0) {
+      int quantity = savedProductItem.get().getQuantity() - productItemReq.getQuantity();
+      if (quantity < 0) {
+        throw new RuntimeException("You can't buy that amount of this product, products left are " + savedProductItem.get().getQuantity());
+      }
+      return quantity;
+    } else {
+      throw new RuntimeException("ProductItem is out of stock");
+    }
+  }
+
+  private ProductOrder getPendingProductOrder(long userId) {
+    List<ProductOrder> pendingProductOrders = repository.findByUserIdAndStatus(userId, ProductOrderStatus.PENDING.status);
+    Optional<ProductOrder> pendingProductOrder = pendingProductOrders.stream().filter(order -> order.getStatus().equals(ProductOrderStatus.PENDING.status)).findFirst();
+    return pendingProductOrder.orElse(null);
+  }
+
+  public String checkoutStripe(ProductOrder productOrder) throws StripeException {
+    // Check quantity here to be able to cancel the order before paying
+    port(4242);
+    Stripe.apiKey = "STRIPE_API_KEY";
+
+    SessionCreateParams.Builder stripeBuilder =
+      SessionCreateParams.builder()
+        .setMode(SessionCreateParams.Mode.PAYMENT)
+        .setSuccessUrl("http://localhost:5173/order-success")
+        .setCancelUrl("http://localhost:5173/order-fail");
+
+    for (OrderItem productOrderItem : productOrder.getOrderItems()) {
+      stripeBuilder.addLineItem(
+        SessionCreateParams.LineItem.builder()
+          .setQuantity((long) productOrderItem.getQuantity())
+          .setPriceData(
+            SessionCreateParams.LineItem.PriceData.builder()
+              .setCurrency("usd")
+              .setUnitAmount(productOrderItem.getProductItem().getProduct().getPrice().multiply(new BigDecimal(100)).longValue())
+              .setProductData(
+                SessionCreateParams.LineItem.PriceData.ProductData.builder()
+                  .setName(productOrderItem.getProductItem().getProduct().getName())
+                  .build())
+              .build())
+          .build());
+    }
+
+    SessionCreateParams stripeParams = stripeBuilder.build();
+
+    Session session = Session.create(stripeParams);
+
+    return session.getUrl();
+  }
+
+  public ProductOrder completeOrder(ProductOrder productOrder) throws Exception {
+    ArrayList<ProductItem> productItemsToUpdate = new ArrayList<>();
+
+    for (OrderItem orderItem : productOrder.getOrderItems()) {
+      int quantity = orderItem.getProductItem().getQuantity() - orderItem.getQuantity();
+
+      if (quantity > 0) {
+        Optional<ProductItem> savedProductItem = productItemRepository.findById(orderItem.getProductItem().getId());
+        if (savedProductItem.isPresent()) {
+          savedProductItem.get().setQuantity(quantity);
+          productItemsToUpdate.add(savedProductItem.get());
         } else {
-            throw new RuntimeException("Product was not found");
+          throw new IOException("ProductItem in order was not found");
         }
-
-        if(savedOrderItem.isPresent()) {
-            savedOrderItem.get().setQuantity(savedOrderItem.get().getQuantity() + productItemReq.getQuantity());
-            BigDecimal orderItemPrice = productPrice.multiply(new BigDecimal(productItemReq.getQuantity()));
-            savedOrderItem.get().addPrice(orderItemPrice);
-            orderItemRepository.save(savedOrderItem.get());
-        } else {
-            OrderItem orderItem = new OrderItem(productItemReq.getQuantity(), productItemReq, productOrderReq, productPrice.multiply(new BigDecimal(productItemReq.getQuantity())));
-            newOrderItem = orderItemRepository.save(orderItem);
-        }
-
-        return newOrderItem;
+      } else {
+        throw new Exception("There is not enough stock for the product: " + orderItem.getProductItem().getProduct().getName());
+      }
     }
 
-    private void updateProductItem(Optional<ProductItem> savedProductItem) {
-        savedProductItem.ifPresent(newProductItem -> productItemRepository.save(savedProductItem.get()));
-    }
+    productItemRepository.saveAll(productItemsToUpdate);
 
+    Optional<ProductOrder> savedProductOrder = repository.findById(productOrder.getId());
 
-    private int getProductItemQuantity(Optional<ProductItem> savedProductItem, ProductItem productItemReq) throws RuntimeException {
-        if(savedProductItem.isPresent() && savedProductItem.get().getQuantity() > 0) {
-            int quantity = savedProductItem.get().getQuantity() - productItemReq.getQuantity();
-            if(quantity < 0) {
-                throw new RuntimeException("You can't buy that amount of this product, products left are " + savedProductItem.get().getQuantity());
-            }
-            return quantity;
-        } else {
-            throw new RuntimeException("ProductItem is out of stock");
-        }
+    if (savedProductOrder.isPresent()) {
+      savedProductOrder.get().setStatus(ProductOrderStatus.COMPLETED.status);
+      repository.save(savedProductOrder.get());
+      return savedProductOrder.get();
+    } else {
+      throw new IOException("order was not found");
     }
-
-    private ProductOrder getPendingProductOrder(long userId) {
-        List<ProductOrder> pendingProductOrders = repository.findByUserIdAndStatus(userId, ProductOrderStatus.PENDING.status);
-        Optional<ProductOrder> pendingProductOrder = pendingProductOrders.stream().filter(order -> order.getStatus().equals(ProductOrderStatus.PENDING.status)).findFirst();
-        return pendingProductOrder.orElse(null);
-    }
+  }
 }

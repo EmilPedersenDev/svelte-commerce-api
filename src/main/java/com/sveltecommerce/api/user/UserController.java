054d2d206b2f5b302255c176db48e445e8dc73d8
RefinedWiki Build Agent <bamboo@refinedwiki.com>
2023-10-05 09:00:03 +0200

    Adding a user

--- src/main/java/com/sveltecommerce/api/user/UserController.java
+++ src/main/java/com/sveltecommerce/api/user/UserController.java
@@ -6,23 +6,45 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
+import javax.ws.rs.Consumes;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 import java.util.List;
 import java.util.Optional;
 
+import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
 
+@CrossOrigin(origins = "http://localhost:5173", maxAge = 3600)
 @RestController
-@RequestMapping("users")
+@Consumes({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
+@RequestMapping("${apiVersion}" + "users")
 public class UserController {
 
-    @Autowired
-    private UserRepository repository;
+  @Autowired
+  private UserRepository repository;
 
-    public UserController(UserRepository repository) {
-        this.repository = repository;
-    }
+  public UserController(UserRepository repository) {
+    this.repository = repository;
+  }
 
-    @GetMapping
-    public ResponseEntity<List<User>> getAllAuthors() {
-        return new ResponseEntity<>(repository.findAll(), HttpStatus.OK);
+  @GetMapping
+  public ResponseEntity<List<User>> getAllAuthors() {
+    return new ResponseEntity<>(repository.findAll(), HttpStatus.OK);
+  }
+
+  @GetMapping("/{id}")
+  @Produces(APPLICATION_JSON)
+  public ResponseEntity getUser(@PathVariable long id) {
+    try {
+      Optional<User> user = repository.findById(id);
+
+      if (user.isPresent()) {
+        return new ResponseEntity<>(user, HttpStatus.OK);
+      } else {
+        throw new RuntimeException("No user was found");
+      }
+    } catch (Exception ex) {
+      throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "error");
     }
+  }
 }
